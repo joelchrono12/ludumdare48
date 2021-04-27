9ef02feaf790625e7d9736429c0a415df9a21f20
joelchrono12 <jisraelgarcia@hotmail.com>
2021-04-26 19:55:22 -0500

    Ludum dare release

--- levels/Goal.gd
+++ levels/Goal.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-
+export var path = "res://levels/World2.tscn"
 func _ready() -> void:
 	pass # Replace with function body.
 
@@ -9,3 +9,9 @@ func _ready() -> void:
 func _on_Area2D_body_entered(body: Node) -> void:
 	if body.name == "Player":
 		body.victory()
+		$ChangeScene.start()
+		
+
+func _on_ChangeScene_timeout() -> void:
+	get_tree().change_scene(path)
+	pass # Replace with function body.
